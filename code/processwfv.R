ae7a5528e9853e78479f90b819acc848f6b37ff7
davidrsch <daviddrsch@gmail.com>
2023-08-19 21:52:00 +0200

    Actualizando

--- code/processwfv.R
+++ code/processwfv.R
@@ -13,171 +13,158 @@ library(lubridate)
 library(dplyr)
 library(quadprog)
 library(Matrix)
-
-#####
-library(quantmod)
-#Downloading IBEX data----
-IBEX <- getSymbols("^IBEX",
-                   from = "1990-01-01",
-                   to = "2023-02-28",
-                   auto.assign = F) |>
-  as.data.frame()
-
-IBEX <- cbind(
-  Date = rownames(IBEX),
-  Close = IBEX$IBEX.Close) |>
-  as.data.frame()
-
-IBEX <- IBEX |>
-  na.omit() |>
-  as.data.frame() |>
-  mutate(
-    Date = strftime(Date, "%Y-%m")) |>
-  group_by(Date) |>
-  summarise(CloseI = last(Close))|>
-  mutate(CloseI = as.numeric(CloseI)) |>
-  as.data.frame()
-######
+library(tibble)
 
 ## Loading data----
-data = read.csv("data/readjwithibxcorr.csv")
+#load("F:/APPs/TFM/data/data.Rdata")
+#load("F:/APPs/TFM/data/modeling.Rdata")
+load("data/results.Rdata")
 
-## Giving format to data
-data <- data |>
-  filter(!ID %in% c(28,77,30,35))
-names(data) <- c(
-  "Date", "X1", "Y1", "X2", "Y2", "X3", "Y3", "X4", "Y4",
-  "X5", "Y5", "X6", "Y6", "X7", "Y7", "ID", "sector", "subsector")
-data <- data |>
-  mutate(Y0 = if_else(Y1 < 0,0,1),
-         X0 = if_else(X1 < 0,0,1))
-head(data)
-
-## Setting parameters ----
-timesteps = 1
-timehorizon = 1
-
-
-data <- data |> 
+data <- bind_rows(vecs3d2e)
+data <- data  |>
   arrange(Date)
-datan <- data
-datan <- datan |>
-  group_by(ID) |>
-  mutate(
-    means = cummean(X1))
-datat <- datan |>
-  ungroup() |>
-  filter(Date >= "2003-01")
-
-listdates <- datat |>
-  select(Date) |>
+inputsinfo <- data|>
+  select(inputs) |>
   pull() |>
-  unique()
+  dim()
+outputsinfo <- data|>
+  select(outputs) |>
+  pull() |>
+  dim()
 
-#set_random_seed(12.3)
 ## Building model ----
 ### Parameters ----
-steps <- 1
-tsperset <- 8
-thsteps <- 1
+steps <- inputsinfo[2]
+tsperset <- inputsinfo[3]
+thsteps <- outputsinfo[2]
 
 ### Model structure ----
 #### Input layers----
 inp <- layer_input(
   shape = c(NULL,steps,tsperset))
-inp2 <- layer_input(
-  shape = c(NULL,steps,4))
-#### Hiden layers----
+#### Hidden layers----
 cnn <- inp |>
   layer_conv_1d(
     filters = 64,
     kernel_size = 1,
-    # kernel_regularizer = regularizer_l1(0.01),
-    # bias_regularizer = regularizer_l1(0.01),
-    activation = layer_activation_leaky_relu()) |>
-  layer_dense(1)
-hidd <- cnn |>
-  layer_concatenate(inp2) |>
+    activation = layer_activation_leaky_relu())
+lstm <- cnn |>
   layer_lstm(64)
 #### Output layers----
-out <- hidd |> 
-  layer_dense(thsteps*1)
+out <- lstm |> 
+  layer_dense(
+    thsteps*1)
 ### Building model---- 
-model <- keras_model(c(inp, inp2),c(cnn, out))
+model <- keras_model(inp, out)
 #### Compiling model ----
 model |> 
-  compile(loss = "mse", optimizer = optimizer_adam())
+  compile(loss = "mse", optimizer = optimizer_sgd(0.0005))
 #### Summary ----  
 model
 
+model |>
+  getconfig() |>
+  plot_modelk() |>
+  grViz()
+
 ## Testing model----
 predictions <- c()
+means <- c()
+listdates <- data |>
+  select(Date) |>
+  pull() |>
+  unique()
 
-nopredm <- 36
-
-for(i in 1:length(listdates)) {
-  datatrain <- datat |>
-    filter(Date <= listdates[i])
+for (i in 1:length(listdates)) {
+  d <- listdates[i]
+  inputs <- data |>
+    filter(Date == d) |>
+    select(inputs) |>
+    pull()
+  inputspred <- data |>
+    filter(Date == d) |>
+    select(inputs) |>
+    pull()
+  outputs <- data |>
+    filter(Date == d) |>
+    select(outputs) |>
+    pull()
+  outputs <- outputs[,,1]
   
-  np <- datat |>
-    filter(Date == listdates[i])
-  np <- dim(np)[1]
-  ### Creating inputs and outputs for train----
-  input_train <- datatrain |> 
-    select(contains("X")) |> 
-    as.matrix()
-  dim(input_train) <- c(dim(input_train)[1],timesteps,dim(input_train)[2])
-  sector <- datatrain |>
-    #  mutate(sector = log(sector)) |> 
-    select(sector)
-  subsector <- datatrain |>
-    #  mutate(subsector = log(subsector)) |>
-    select(subsector)
-  #sectors <- abind(sector, subsector, along = 3)
-  #input_train <- abind(input_train, sectors, along = 3)
-  output_signt <- datatrain |> 
-    select(Y0) |> 
-    as.matrix()
-  dim(output_signt) <- c(dim(output_signt)[1],timehorizon,dim(output_signt)[2])
-  output_train <- datatrain |> 
-    select(Y1) |> 
-    as.matrix()
+  ids <- data |> 
+    filter(Date == d) |>
+    select(ID) |>
+    pull()
   
-  if(i > nopredm){
-    diminp <- dim(input_train)[1]
-    top <- (diminp-np+1):diminp
+  if(i==1){
+    dfmeans <- inputspred[,,1] |>
+      as.data.frame() |>
+      cbind(ID = ids)
+  }else{
+    dfmeansupd <- inputspred[,dim(inputs)[2],1] |>
+      as.data.frame() |>
+      cbind(ID = ids)
+    names(dfmeansupd)[1] <- paste0("V",(dim(dfmeans)[2]))
+    idsdf <- unique(c(ids, dfmeans$ID))
+    idsdf <- data.frame(
+      ID = idsdf
+    )
+    dfmeansupd <- dplyr::left_join(idsdf, dfmeansupd, by = "ID")
+    ifelse(
+      dim(dfmeansupd)[1] > dim(dfmeans)[1],
+      dfmeans <- dplyr::left_join(dfmeansupd, dfmeans, by = "ID"),
+      dfmeans <- dplyr::left_join(dfmeans, dfmeansupd, by = "ID")
+    )
+  }
+  
+  if(i > 1){
     pred <- model |> predict(
-      list(
-        input_train[top,,,drop = F],
-        input_train[top,, c(1:4), drop=F]
-      ))
-    pred <- pred[[2]]
+      list(inputspred))
+    if(length(which(pred==0)>10)){
+      stop()
+    }
     predictions <- rbind(predictions, pred)
+    MEANS <-  dfmeans |>
+      rowwise() |>
+      mutate(
+        means = mean(c_across(-ID), na.rm = T)
+      ) |>
+      slice(
+        match(ids,ID)
+      ) |>
+      pull(means) |>
+      as.matrix()
+    means <- rbind(means, MEANS)
   }
   
   ## Training the model ----
   model |>
     fit(
-      x = list(
-        input_train, 
-        input_train[,, c(1:4), drop=F]
-      ),
-      y = list(
-        output_signt,
-        output_train
-      ),
+      inputs,
+      outputs,
       epochs = 1,
-      batch_size = 8,
+      batch_size = 10,
       shuffle = F,
-      verbose = 0)
-  
+      verbose = 0
+    )
 }
 
 ## Evaluating ----
 #### Values that performed badly ----
-datat |>
-  filter(Date != listdates[1]) |>
-  cbind(predictions = predictions) |>
+y1 <- data |>
+  filter(
+    Date > listdates[1]
+  ) |>
+  select(outputs) |>
+  pull()
+y1 <- y1[,,1]
+data |>
+  filter(
+    Date > listdates[1]
+  ) |>
+  cbind(predictions = predictions[,1]) |>
+  cbind(means = means) |>
+  cbind(Y1 = y1) |>
   group_by(ID) |>
   summarise(
     mse_model = mse(predictions, Y1),
@@ -187,10 +174,15 @@ datat |>
     r2_vsgmean = 1 - (sum((Y1 - predictions)^2)/sum((Y1 - mean(Y1))^2))
   ) |>
   dplyr::filter(r2_vsactmean < 0)
+
 #### Mean R^2 model----
-arsqrd <- datat |> 
-  filter(Date != listdates[1]) |>
-  cbind(predictions = predictions) |>
+arsqrd <- data |>
+  filter(
+    Date > listdates[1]
+  ) |>
+  cbind(predictions = predictions[,1]) |>
+  cbind(means = means) |>
+  cbind(Y1 = y1) |>
   group_by(ID) |>
   summarise(
     mse_model = mse(predictions, Y1),
@@ -204,15 +196,21 @@ arsqrd <- datat |>
   pull() |>
   mean()
 
-Yhat <- datat |>
-  filter(Date != listdates[1]) |>
-  mutate(yhat = predictions) |>
-  mutate(
-    Real = Y1,
-    IBEX = Y2,
-    Yhat = yhat
+
+Yhat <- data |>
+  left_join(IBEXsel, by ="Date") |>
+  mutate(IBEX = Return_I) |>
+  arrange(Date) |>
+  filter(
+    Date > listdates[1]
   ) |>
-  select(Date, Real, IBEX, Yhat, ID)
+  mutate(yhat = predictions[,1]) |>
+  mutate(
+    Real = y1,
+    Yhat = yhat,
+    Means = means
+  ) |>
+  select(Date, Real, IBEX, Yhat, Means, ID)
 
 pvtYhat <- Yhat |>
   select(Date, Yhat, ID) |>
@@ -221,114 +219,204 @@ pvtYhat <- Yhat |>
     values_from = Yhat
   )
 
-weights <- data.frame()
-
-listdates[-1] |>
-  lapply(function(x,
-                  noeval = listdates[2],
-                  data = pvtYhat){
-    if(x != noeval){
-      data <- data |>
-        filter(Date <= x)
-      nare <- which(is.na(data[dim(data)[1],]))
-      naremo <- which(is.na(data[(dim(data)[1]-1),]))
-      nare <- c(nare,naremo)
-      nare <- unique(nare)
-      if(length(nare) != 0){
-        cartera <- data[, - nare]
-      }else{
-        cartera <- data
-      }
-      return <- cartera[dim(cartera)[1], -1] |>
-        as.matrix() |>
-        t()
-      covm <- cov(cartera[, -1], use = "complete.obs")
-      npcovm <- nearPD(covm)$mat |> 
-        as.matrix()
-      n <- ncol(npcovm)
-      D.mat <- npcovm 
-      d.vec <- rep(0,n) 
-      A.mat <- matrix(1,nrow=nrow(D.mat))
-      A.mat <- cbind(A.mat,d.vec)
-      A.mat <- cbind(A.mat,diag(nrow(D.mat)))
-      b.vec <- c(1,0,rep(0,nrow(D.mat)))
-      qp.out <- solve.QP(Dmat=D.mat, dvec=d.vec,
-                         Amat=A.mat, bvec=b.vec, meq=1)
-      
-      weig <- round(qp.out$solution, 3)
-      for(i in 1:length(weig)){
-        if(weig[i] < 0.001){
-          weig[i] <- 0
-        }else{}
-      }
-      weig <- as.data.frame(t(weig))
-      names(weig) <- names(cartera[, -1])
-      weights <<- bind_rows(weights, weig)
-    }
-    
-  }) |> 
-  invisible()
-
 pvtReal <- Yhat |>
   select(Date, Real, ID) |>
   pivot_wider(
     names_from = ID,
     values_from = Real
-  ) |>
-  filter(Date >= listdates[1]) |>
+  )
+
+pvtMeans <- Yhat |>
+  select(Date, Means, ID) |>
+  pivot_wider(
+    names_from = ID,
+    values_from = Means
+  )
+
+weightse <- data.frame()
+weightsm <- data.frame()
+
+for (i in 1:length(listdates[-1])) {
+  if(i>1){
+    
+    dataeQP <- pvtReal |>
+      filter(Date < listdates[-1][i]) |>
+      rbind(pvtYhat |>
+              filter(Date == listdates[-1][i])
+            )
+    datamQP <- pvtReal |>
+      filter(Date < listdates[-1][i]) |>
+      rbind(pvtMeans |>
+              filter(Date == listdates[-1][i])
+      )
+    nare <- which(is.na(dataeQP[dim(dataeQP)[1],]))
+    naremo <- which(is.na(dataeQP[(dim(dataeQP)[1]-1),]))
+    nare <- c(nare,naremo)
+    nare <- unique(nare)
+    if(length(nare) != 0){
+      carterae <- dataeQP[, - nare]
+      carteram <- datamQP[, - nare]
+    }else{
+      carterae <- dataeQP
+      carteram <- datamQP
+    }
+    returne <- carterae[dim(carterae)[1], -1] |>
+      as.matrix() |>
+      t()
+    returnm <- carteram[dim(carteram)[1], -1] |>
+      as.matrix() |>
+      t()
+    covme <- cov(carterae[, -1], use = "complete.obs")
+    npcovme <- nearPD(covme)$mat |> 
+      as.matrix()
+    covmm <- cov(carteram[, -1], use = "complete.obs")
+    npcovmm <- nearPD(covmm)$mat |> 
+      as.matrix()
+    n <- ncol(npcovme)
+    
+    qp_oute <- solve.QP(
+      Dmat = 2*npcovme,
+      dvec = rep(0,n),
+      Amat = cbind(-1, diag(n)),
+      bvec = c(-1, rep(0,n)),
+      meq = 1)
+    qp_outm <- solve.QP(
+      Dmat = 2*npcovmm,
+      dvec = rep(0,n),
+      Amat = cbind(-1, diag(n)),
+      bvec = c(-1, rep(0,n)),
+      meq = 1)
+    qp_oute <- qp_oute$solution
+    qp_oute <- floor(qp_oute*100)/100
+    qp_outm <- qp_outm$solution
+    qp_outm <- floor(qp_outm*100)/100
+    for(j in 1:length(qp_oute)){
+      if(qp_oute[j] < 0.001){
+        qp_oute[j] <- 0
+      }else{}
+      if(qp_outm[j] < 0.001){
+        qp_outm[j] <- 0
+      }else{}
+    }
+    names(qp_oute) <- names(carterae[, -1])
+    names(qp_outm) <- names(carteram[, -1])
+    weightse <- bind_rows(weightse, qp_oute)
+    weightsm <- bind_rows(weightsm, qp_outm)
+  }
+}
+
+pvtReal <- pvtReal |>
   select(-Date)
 
 pvtReal[is.na(pvtReal)] <- 0
-weights[is.na(weights)] <- 0
+weightse[is.na(weightse)] <- 0
+weightsm[is.na(weightsm)] <- 0
+resultscnnlstmssing2[["resultspvtReal"]] <- pvtReal
 
-returndf <- pvtReal[-1,] * weights
-returndf <- rowSums(returndf)
+returndfe <- pvtReal[-1,] * weightse
+returndfe <- rowSums(returndfe)
+returndfm <- pvtReal[-1,] * weightsm
+returndfm <- rowSums(returndfm)
+mg <- 1
 returndf <- data.frame(
-  Date = listdates[-c(1:2)],
-  Portre = returndf,
-  IBEX = IBEX |>
-    mutate(CloseI = Delt(CloseI)) |>
-    filter(Date >= listdates[3]) |>
+  Date = listdates[-c(1:mg)],
+  Portre = c(1,returndfe[mg:length(returndfe)]),
+  Means = c(1,returndfm[mg:length(returndfm)]),
+  IBEX = c(1, IBEXsel |>
+    filter(Date > listdates[mg+1]) |>
     select(2) |>
-    pull()
+    pull())
 ) |>
   rename(
-    IBEX = 3
+    IBEX = 4
   )
+resultscnnlstmssing2[["resultsMEANS"]] <- returndf$Means
+
 returndf <- returndf |>
   mutate(
     Portreinv = cumsum(Portre),
+    Meansinv = cumsum(Means),
     IBEXinv = cumsum(IBEX)
-  ) |>
-  mutate(
-    Portreinv = Portreinv + 1,
-    IBEXinv = IBEXinv + 1
   )
 
 library(ggplot2)
 returndf |>
   mutate(
-    Date = ym(Date)) |>
+    Date = as.Date(Date)) |>
   ggplot(aes(x = Date, group = 1)) +
   geom_line(aes(y = Portreinv), color = "blue") +
+  geom_line(aes(y = Meansinv), color = "green") +
   geom_line(aes(y = IBEXinv), color = "red") +
   theme(axis.text.x = element_text(angle = 90))
 
-# returndf |>
-#   filter(Date >= "2020-01") |>
-#   mutate(
-#     Portreinv = cumsum(Portre),
-#     IBEXinv = cumsum(IBEX)
-#   ) |>
-#   mutate(
-#     Portreinv = Portreinv + 1,
-#     IBEXinv = IBEXinv + 1
-#   ) |>
-#   mutate(
-#     Date = ym(Date)) |>
-#   ggplot(aes(x = Date, group = 1)) +
-#   geom_line(aes(y = Portreinv), color = "blue") +
-#   geom_line(aes(y = IBEXinv), color = "red") +
-#   theme(axis.text.x = element_text(angle = 90))
+returndf$Portreinv[212-mg+1]-returndf$Meansinv[212-mg+1]
 
-arsqrd
+#names(resultscnnlstmssing1$resultsweightse) <- c("001","002","003",
+#                                          "004","005","006",
+#                                          "007","008","009",
+#                                          "010")
+
+try <- 10
+if(try<10){
+  try <- paste0("00",try)
+}else if(try>10 && try<100){
+  try <- paste0("0",try)
+}
+
+tryresults <- data |>
+  filter(Date > listdates[1]) |>
+  cbind(predictions = predictions) |>
+  cbind(means = means) |>
+  mutate(Y1 = y1) |>
+  select(Date, predictions, means, Y1) |>
+  group_by(Date) |>
+  summarise(
+    rsqrd = 1 - (sum((Y1 - predictions)^2)/sum((Y1 - means)^2)),
+    mse = mse(predictions, Y1),
+  ) |>
+  left_join(returndf, by = 'Date') |>
+  select(Date, Portre, IBEX, rsqrd, mse)
+
+resultscnnlstmssing3$resultsD[[try]] <- tryresults
+
+tryresultsID <- data |>
+  filter(Date > listdates[1]) |>
+  cbind(predictions = predictions) |>
+  cbind(means = means) |>
+  mutate(Y1 = y1) |>
+  select(Date, predictions, means, Y1, ID) |>
+  group_by(ID) |>
+  summarise(
+    rsqrd = 1 - (sum((Y1 - predictions)^2)/sum((Y1 - means)^2)),
+    mse = mse(predictions, Y1)
+  ) |>
+  select(ID, rsqrd, mse)
+
+resultscnnlstmssing3$resultsID[[try]] <- tryresultsID
+
+trypredictions <- pvtYhat |>
+  mutate_if(is.double, as.double)
+trypredictions
+resultscnnlstmssing3$resultspredictions[[try]] <- trypredictions
+
+tryweightse <- tryresults |>
+  filter(Date > Date[1]) |>
+  select(Date) |>
+  cbind(weightse)
+
+resultscnnlstmssing3$resultsweightse[[try]] <- tryweightse
+
+save(
+  resultscnnlstmssing1,
+  resultscnnlstmssing2,
+  resultscnnlstmssing3,
+  file = "data/results.Rdata"
+)
+
+ # resultscnnlstmssing3 <- list()
+ # resultscnnlstmssing3[["resultsD"]] <- list()
+ # resultscnnlstmssing3[["resultsID"]] <- list()
+ # resultscnnlstmssing3[["resultspredictions"]] <- list()
+ # resultscnnlstmssing3[["resultsweightse"]] <- list()
+ # resultscnnlstmssing3[["resultsweightsm"]] <- weightsm
+
